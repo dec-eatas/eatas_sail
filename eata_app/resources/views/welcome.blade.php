d0e6377a60fdfc6d91aa28f47614e487e6649185
zum-m <89333894+zum-m@users.noreply.github.com>
2022-02-07 17:43:41 +0900

    [add] breezeで認証機能を実装しました。

--- eata_app/resources/views/welcome.blade.php
+++ eata_app/resources/views/welcome.blade.php
@@ -20,13 +20,13 @@
             }
         </style>
     </head>
-    larave sail で eata_sail が立ち上がってるよ。三浦
+    <p> larave sail で eata_sail が立ち上がってるよ。三浦</p>
     <body class="antialiased">
         <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
             @if (Route::has('login'))
                 <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
                     @auth
-                        <a href="{{ url('/home') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Home</a>
+                        <a href="{{ url('/dashboard') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Dashboard</a>
                     @else
                         <a href="{{ route('login') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Log in</a>
 
